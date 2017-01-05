3cd822f1d3afa4c14056d0d7b5e17fa2a962b403
John Smith Paraggua <smitparaggua@gmail.com>
2017-01-05 16:06:57 +0800

    Added support for Payment Vault Customers API

--- test/paymaya/payment-vault/integration/customers.spec.js
+++ test/paymaya/payment-vault/integration/customers.spec.js
@@ -1,11 +1,47 @@
+var _ = require('lodash');
 var rek = require('rekuire');
 var paymentVault = rek('test-helper').createPaymentVault();
 
 describe('Customers', () => {
 	it('allows creation of customer in card vault', () => {
 		var payload = buildCreateCustomerPayload();
-		paymentVault.customer.create(payload)
-			.should.eventually.include(payload);
+		return paymentVault.customer.create(payload)
+			.should.eventually.include.keys(
+				'id', 'firstName', 'middleName', 'lastName', 'birthday', 'sex',
+				'contact', 'billingAddress'
+			).then(customer => paymentVault.customer.destroy(customer.id))
+	});
+
+	context('Operations on existing customers', () => {
+		var customer;
+
+		beforeEach(() => {
+			var payload = buildCreateCustomerPayload();
+			return paymentVault.customer.create(payload)
+				.then(created => customer = created);
+		});
+
+		afterEach(() => {
+			return paymentVault.customer.destroy(customer.id);
+		});
+
+		it('allows retrieval of customer', () => {
+			return paymentVault.customer.retrieve(customer.id)
+				.should.eventually.become(customer);
+		});
+
+		it('allows updating customer', () => {
+			var update = {
+				contact: {
+					phone: "+63(2)1234567777",
+					email: "test@example.com"
+				}
+			}
+			return paymentVault.customer.update(customer.id, update)
+				.then(updated => {
+					updated.contact.should.deep.equal(update.contact);
+				});
+		});
 	});
 
 	function buildCreateCustomerPayload() {

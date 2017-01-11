aaf56210877b2f1c6af6cf70079e2d0f0d4eb5e8
John Smith Paraggua <smitparaggua@gmail.com>
2017-01-11 14:27:04 +0800

    Added payment vault webhooks

---
+++ test/paymaya/payment-vault/integration/webhooks.spec.js
@@ -0,0 +1,11 @@
+var rek = require('rekuire');
+var paymaya = rek('index');
+
+// TODO: create test for webhooks
+// webhooks collide in names but are queried using IDs
+xdescribe('Webhooks', () => {
+	it('allows creation of webhooks', () => {
+
+	});
+});
+
